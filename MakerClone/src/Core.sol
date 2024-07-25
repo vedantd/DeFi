941add397fabf737923432333998b99d49285f8d
Vedant Dalvi <vedantdalvi20@gmail.com>
2024-07-26 01:29:10 +0530

    Core spec

---
+++ MakerClone/src/Core.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// SPEC
+// Deposit collateral mint dai (wETH,wBTC)
+// Repay DAI
+// Withdraw collateral
+// Liquidate
+// threshold 150% (collateral value / dai value)
+pragma solidity ^0.8.19;
+
+contract Core {
+    constructor() {}
+}
